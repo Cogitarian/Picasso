eb3fec50bb5d0fe05bfacface509798514728108
Lukasz Prokulski <prokulski@gmail.com>
2017-09-27 20:13:19 +0000

    przykładowy obrazek - w sumie pełny kod

--- analiza.R
+++ analiza.R
@@ -1,3 +1,108 @@
+library(tidyverse)
+library(jpeg)
+library(reshape2)
+
+file <- "pics/female-nude-in-the-garden-1934.jpg"
+picture <- readJPEG(file)
+
+dim(picture)
+dims <- dim(picture)
+
+plot.new()
+rasterImage(picture, 0, 0, 1, 1)
+
+
+image.R <- melt(picture[,,1])
+image.G <- melt(picture[,,2])
+image.B <- melt(picture[,,3])
+
+image.df <- cbind(image.R, image.G[3], image.B[3])
+colnames(image.df) <- c("x","y","R","G","B")
+
+rm(image.R, image.G, image.B, picture)
+
+image.df <- image.df %>%
+   mutate(R = round(R * 255),
+          G = round(G * 255),
+          B = round(B * 255)) %>%
+   mutate(RGB_hex = sprintf("#%02X%02X%02X", R, G, B))
+
+
+image_df_kmeans <- kmeans(image.df[, c("R", "G", "B")], 16)
+
+image.df_centers <- image_df_kmeans$centers %>%
+   round(0) %>%
+   as_data_frame() %>%
+   mutate(cluster = as.numeric(rownames(.))) %>%
+   rename(R_kmeans = R, G_kmeans = G, B_kmeans = B) %>%
+   mutate(RGB_kmeans_hex = sprintf("#%02X%02X%02X", R_kmeans, G_kmeans, B_kmeans))
+
+image.df_all <- image.df %>%
+   mutate(cluster = image_df_kmeans$cluster) %>%
+   left_join(image.df_centers, by = "cluster")
+
+
+head(image.df_all)
+
+popular_colors <- image.df_all %>%
+   count(RGB_hex) %>%
+   ungroup() %>%
+   top_n(32, n) %>%
+   arrange(n)
+
+popular_colors_palete <- as.character(popular_colors$RGB_hex)
+names(popular_colors_palete) <- as.character(popular_colors$RGB_hex)
+
+
+popular_colors %>%
+   mutate(RGB_hex = factor(RGB_hex, levels = RGB_hex)) %>%
+   ggplot() +
+   geom_col(aes(RGB_hex, n, fill = RGB_hex), show.legend = TRUE) +
+   coord_flip() +
+   scale_fill_manual(values = popular_colors_palete)
+
+
+image.df_all %>%
+   count(RGB_kmeans_hex) %>%
+   ungroup() %>%
+   top_n(32) %>%
+   arrange(desc(n))
+
+
+# to view image after color quantization - unncomment below lines
+# # reconstitute the segmented image in the same shape as the input image
+image.segmented <- array(dim=dims)
+image.segmented[,,1] <- matrix(image.df_all$R_kmeans, nrow=dims[1])
+image.segmented[,,2] <- matrix(image.df_all$G_kmeans, nrow=dims[1])
+image.segmented[,,3] <- matrix(image.df_all$B_kmeans, nrow=dims[1])
+image.segmented <- image.segmented/255
+
+# View the result
+plot.new()
+rasterImage(image.segmented, 0, 0, 1, 1)
+
+
+pca <- princomp(image.df_all[, c("R", "G", "B")])
+pca <- pca$scores
+colnames(pca) <- c("PC1", "PC2", "PC3")
+
+normalize <- function(x) {
+   return((x-min(x))/(max(x)-min(x)))
+}
+
+pca <- normalize(pca)
+
+image.pca <- array(dim=dims)
+image.pca[,,1] <- matrix(pca[, 1], nrow=dims[1])
+image.pca[,,2] <- matrix(pca[, 2], nrow=dims[1])
+image.pca[,,3] <- matrix(pca[, 3], nrow=dims[1])
+
+# View the result
+plot.new()
+rasterImage(image.pca, 0, 0, 1, 1)
+
+
+
 # wez kazdy obraz po kolei
 # przeksztalc bitmape na ramke x, y, r, g, b
 # scal rgb do kolorow w hex
