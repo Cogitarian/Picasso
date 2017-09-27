a941f313cf617303b5cc71160a709c9a41831498
Lukasz Prokulski <prokulski@gmail.com>
2017-09-27 14:32:31 +0000

    plan na analize

---
+++ analiza.R
@@ -0,0 +1,10 @@
+# wez kazdy obraz po kolei
+# przeksztalc bitmape na ramke x, y, r, g, b
+# scal rgb do kolorow w hex
+# zrob kmeans() do 32 kolorow z r, g, b
+# policz najbardziej dominujacy kolor - wg hex i wg kmeans
+# zapisz w ramce rok, dominujacy kolor kmeans i kolor hex
+# narysuj barplot rok, 1, fill=kolor w wersji z hex i wersji z kmeans
+# w wiki sprawdz kiedy byl jaki okres, dodaj obpowiednie linie na wykresach
+# wez jakis charakretystyczny obraz i zrob mu rozne filtry :)
+# na przyklad PCA na kolorach :)
